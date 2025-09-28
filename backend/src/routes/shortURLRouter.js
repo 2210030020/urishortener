a154f848402b773b0eda60014b823b4a316fe694
Manvitha Reddy <154338100+2210030020@users.noreply.github.com>
2025-09-28 20:00:29 +0530

    intial commit

--- backend/src/routes/shortURLRouter.js
+++ backend/src/routes/shortURLRouter.js
@@ -1,1 +1,7 @@
+import shortUrlController from '../controllers/shortUrlController.js'
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+const shortURLRouter = Router();
 
+shortURLRouter.post("/",authMiddleware, shortUrlController)
+
+export default shortURLRouter;
