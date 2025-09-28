6211c64968f2944e8ca42adde4570764231083f3
Manvitha Reddy <154338100+2210030020@users.noreply.github.com>
2025-09-28 19:54:24 +0530

    intial commit

--- backend/src/middlewares/authMiddleWare.js
+++ backend/src/middlewares/authMiddleWare.js
@@ -1,1 +1,30 @@
+ import jwt from 'jsonwebtoken';
 
+export const authMiddleware = async (req, res, next) => {
+    try {
+        const cookies = req.cookies;
+       const token = cookies['jwt'];
+         if (!token) {
+            return res.ststus(403).json({
+                message:"Token is Invalid",
+            });
+         }
+         try {
+            const decode =await jwt.decode(token);
+            req.user = decode;
+            console.log("Printing the value of decode--> ",decode);
+            
+         } catch (error) {
+            console.error("Invalid token", error.message);
+            return res.ststus(403).json({
+                message:"Token is Invalid",
+            });
+            next();
+            
+         }
+
+    } catch (error) {
+        return res.status(500).send(error.message);
+
+    }
+}
