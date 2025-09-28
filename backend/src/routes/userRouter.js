3bca8537c205cad09dce4de98b94fac876794a71
Manvitha Reddy <154338100+2210030020@users.noreply.github.com>
2025-09-28 20:01:22 +0530

    intial commit

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,1 +1,12 @@
+/ src/routes/userRouter.js (or .ts)
+import { Router } from 'express';
+import { getProfileOfUser, getMyUrls} from '../controllers/userController.js'
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
+const userRouter = Router();
+
+userRouter.get('/me',authMiddleware, getProfileOfUser);
+
+userRouter.get('/my/url',authMiddleware, getMyUrls);
+
+export default userRouter;
